2d73b213d4a037f76d098dfe2399b45b64ec78b1
WilliamsCF <81392310+WilliamsCF@users.noreply.github.com>
2022-11-15 11:08:50 +0100

    v0.0.2
    update

--- topachat/topachat/src/main/java/com/jee/topachat/TopachatApplication.java
+++ topachat/topachat/src/main/java/com/jee/topachat/TopachatApplication.java
@@ -2,11 +2,13 @@ package com.jee.topachat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class TopachatApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TopachatApplication.class, args);
 	}
 
---
+++ topachat/topachat/src/main/resources/templates/js/index.js
@@ -0,0 +1,271 @@
+stockCG = [];
+stockPROC = [];
+stockRAM = [];
+
+
+fetch("../php/products/product_database.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        stockCG = data["products"]["stockCG"];
+        stockPROC = data["products"]["stockPROC"];
+        stockRAM = data["products"]["stockRAM"];
+    });
+
+
+
+function displayProducts(product) {
+
+    if (product === 'cg') {
+        // On affiche les cartes graphiques
+        document.getElementById('cg').style.display = 'block';
+        // On cache les autres catégories de produits
+        document.getElementById('proc').style.display = 'none';
+        document.getElementById('ram').style.display = 'none';
+    }
+
+    if (product === 'proc') {
+        // On affiche les processeurs
+        document.getElementById('proc').style.display = 'block';
+        // On cache les autres catégories de produits
+        document.getElementById('cg').style.display = 'none';
+        document.getElementById('ram').style.display = 'none';
+    }
+
+    if (product === 'ram') {
+        // On affiche les cartes graphiques
+        document.getElementById('ram').style.display = 'block';
+        // On cache les autres catégories de produits
+        document.getElementById('proc').style.display = 'none';
+        document.getElementById('cg').style.display = 'none';
+    }
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {
+        accordion: true
+    });
+
+});
+
+function hideStock(product_id) {
+    const elmt = document.getElementById('product_' + product_id + '_price');
+    const current_state = elmt.getAttribute('style').split(';')[0].split(':')[1];
+    console.log(current_state);
+    if (current_state === ' none') {
+        elmt.style.display = 'block';
+    } else {
+        elmt.style.display = 'none';
+    }
+}
+
+function showCurrentStock(product_type, product_id) {
+
+    const elmt = document.getElementById('product_' + product_id + '_price');
+
+    if (product_type === 'cg') {
+
+        stockCG.forEach(value => {
+            if (value.id === product_id) {
+                elmt.innerHTML = value.stock + ' pièce(s) en stock !';
+            }
+        });
+    }
+
+    if (product_type === 'proc') {
+
+        stockPROC.forEach(value => {
+            if (value.id === product_id) {
+                elmt.innerHTML = value.stock + ' pièce(s) en stock !';
+            }
+        });
+    }
+
+    if (product_type === 'ram') {
+
+        stockRAM.forEach(value => {
+            if (value.id === product_id) {
+                elmt.innerHTML = value.stock + ' pièce(s) en stock !';
+            }
+        });
+    }
+}
+
+//Zoom
+//Modal pour permettre le zoom
+const modal = document.getElementById('myModal');
+const modalImg = document.getElementById("img01");
+const captionText = document.getElementById("caption");
+var img = document.querySelectorAll('.thumbnail');
+    
+for (var i=0; i<img.length; i++){
+    
+    img[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+     
+}
+// Fermeture
+modal.onclick = function() {
+    img01.className += " out";
+    setTimeout(function() {
+       modal.style.display = "none";
+       img01.className = "modal-content";
+     }, 400);
+    
+ }       
+
+function changeQuantity(type, product_id, product_type) {
+    const p_id = product_id.split('_')[1];
+    if (type === '+') {
+        if (product_type === 'cg') {
+            stockCG.forEach(value => {
+                if (value.id === p_id) {
+                    const max_stock = value.stock;
+                    const current_Value = document.getElementById(product_id).value;
+                    if (current_Value < max_stock) {
+                        document.getElementById(product_id).value = parseInt(current_Value) + 1;
+                    }
+                }
+            });
+
+        } else if (product_type === 'proc') {
+            stockPROC.forEach(value => {
+                if (value.id === p_id) {
+                    const max_stock = value.stock;
+                    const current_Value = document.getElementById(product_id).value;
+                    if (current_Value < max_stock) {
+                        document.getElementById(product_id).value = parseInt(current_Value) + 1;
+                    }
+                }
+            });
+
+        } else if (product_type === 'ram') {
+            stockRAM.forEach(value => {
+                if (value.id === p_id) {
+                    const max_stock = value.stock;
+                    const current_Value = document.getElementById(product_id).value;
+                    if (current_Value < max_stock) {
+                        document.getElementById(product_id).value = parseInt(current_Value) + 1;
+                    }
+                }
+            });
+
+        }
+    }
+
+    if (type === '-') {
+        if (product_type === 'cg') {
+            stockCG.forEach(value => {
+                if (value.id === p_id) {
+                    const current_Value = document.getElementById(product_id).value;
+                    if (current_Value > 0) {
+                        document.getElementById(product_id).value = parseInt(current_Value) - 1;
+                    }
+                }
+            });
+
+        } else if (product_type === 'proc') {
+            stockPROC.forEach(value => {
+                if (value.id === p_id) {
+                    const current_Value = document.getElementById(product_id).value;
+                    if (current_Value > 0) {
+                        document.getElementById(product_id).value = parseInt(current_Value) - 1;
+                    }
+                }
+            });
+
+        } else if (product_type === 'ram') {
+            stockRAM.forEach(value => {
+                if (value.id === p_id) {
+                    const current_Value = document.getElementById(product_id).value;
+                    if (current_Value > 0) {
+                        document.getElementById(product_id).value = parseInt(current_Value) - 1;
+                    }
+                }
+            });
+
+        }
+    }
+}
+
+
+
+function register() {
+    document.getElementById("register_form").style.display = "block";
+    document.getElementById("login_form").style.display = "none";
+
+
+}
+
+function logout() {
+    document.getElementById("register_form").style.display = "none";
+    document.getElementById("login_form").style.display = "block";
+    document.getElementById("user_details").style.display = "none";
+
+}
+
+// Variable to hold request
+var request;
+
+// Bind to the submit event of our form
+$("#basket_form").submit(function(event){
+
+    // Prevent default posting of form - put here to work in case of errors
+    event.preventDefault();
+
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        url: "/index.php?page=products1",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+});
