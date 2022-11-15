78a7f45fbfdf7dcf61d47d8f52b6719aa8d615ff
WilliamsCF <81392310+WilliamsCF@users.noreply.github.com>
2022-11-15 10:28:10 +0100

    v0.0.1
    default project

---
+++ topachat/topachat/src/main/java/com/jee/topachat/TopachatApplication.java
@@ -0,0 +1,13 @@
+package com.jee.topachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopachatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopachatApplication.class, args);
+	}
+
+}
---
+++ topachat/topachat/src/test/java/com/jee/topachat/TopachatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jee.topachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
