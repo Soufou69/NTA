a735cdcaa9b1c9571faa1edb91aeccb728ea2d30
WilliamsCF <81392310+WilliamsCF@users.noreply.github.com>
2022-11-15 12:58:31 +0100

    v0.0.3
    update car c'est de la merde

---
+++ topachat_old/topachat/src/main/java/com/jee/topachat/servingwebcontent/TopachatApplication.java
@@ -0,0 +1,15 @@
+package com.jee.topachat.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TopachatApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TopachatApplication.class, args);
+	}
+
+}
