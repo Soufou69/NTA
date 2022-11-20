9b912a88774619bbe5091b7635f1fe595dca57f0
ZeWills <40826984+ZeWills@users.noreply.github.com>
2022-11-20 17:03:31 +0100

    v0.0.1
    W Branch

--- src/main/java/com/jee/NTA/NtaApplication.java
+++ src/main/java/com/jee/NTA/NtaApplication.java
@@ -1,4 +1,4 @@
-package com.example.NTA;
+package com.jee.NTA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/jee/NTA/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.jee.NTA.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+
+}
