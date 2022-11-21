b603a73cda24ea4a8b48ac91ddfd29ce8aec6ed8
Chautard Corentin <co.chautard@gmail.com>
2022-11-21 11:18:48 +0100

    jee

--- src/main/java/com/jee/NTA/VehicleController.java
+++ src/main/java/com/jee/NTA/VehicleController.java
@@ -1,4 +1,4 @@
-package com.example.NTA;
+package com.jee.NTA;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
