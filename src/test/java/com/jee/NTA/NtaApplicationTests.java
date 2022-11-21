b89623b34831f26171a25ba27239466e1d2b1369
Soufou69 <soufouirl@outlook.fr>
2022-11-21 11:08:15 +0100

    change package name

---
+++ src/test/java/com/jee/NTA/NtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jee.NTA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
