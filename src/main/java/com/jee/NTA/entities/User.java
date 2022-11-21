78628058bb16598a6b09219739dc10ef451fc219
Soufou69 <soufouirl@outlook.fr>
2022-11-21 11:14:22 +0100

    rebase

---
+++ src/main/java/com/jee/NTA/entities/User.java
@@ -0,0 +1,25 @@
+package com.jee.NTA.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    private int id;
+
+    private String name;
+    private String password;
+    private boolean isAdmin;
+
+    @OneToMany
+    private List<Commande> commandes;
+
+
+
+
+
+
+}
---
+++ src/main/java/com/jee/NTA/entities/typeProduit.java
@@ -0,0 +1,5 @@
+package com.jee.NTA.entities;
+
+public enum typeProduit {
+    CG,PROC,RAM
+}
