63d672ecdb536161950094ba383a3481b4e9fb4a
Soufou69 <soufouirl@outlook.fr>
2022-11-23 02:10:51 +0100

    Minor changes (just in case)

--- src/main/java/com/jee/NTA/NtaApplication.java
+++ src/main/java/com/jee/NTA/NtaApplication.java
@@ -2,10 +2,8 @@ package com.jee.NTA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class NtaApplication {
 
 	public static void main(String[] args) {
