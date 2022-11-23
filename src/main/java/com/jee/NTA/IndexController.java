48c953a9eac1127b2052840f176d2bf4ed78de0d
Soufou69 <soufouirl@outlook.fr>
2022-11-23 02:09:17 +0100

    Update DB connect config & creating first Controller

--- src/main/java/com/jee/NTA/IndexController.java
+++ src/main/java/com/jee/NTA/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-class VehicleController {
+class IndexController {
 
     @GetMapping(value = "/")
     String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
--- src/main/java/com/jee/NTA/NtaApplication.java
+++ src/main/java/com/jee/NTA/NtaApplication.java
@@ -2,8 +2,10 @@ package com.jee.NTA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class NtaApplication {
 
 	public static void main(String[] args) {
